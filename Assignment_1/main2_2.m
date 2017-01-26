fe59f8e57fd20ac88f5aa298672703704a3c6e41
Weery <vicnilss@student.chalmers.se>
2017-01-26 16:40:25 +0100

    Wrote prior/posterior distribution functions

--- Assignment_1/main2_2.m
+++ Assignment_1/main2_2.m
@@ -9,3 +9,24 @@
 % Bjarki Vilmarsson
 % bjarkiv@student.chalmers.se
 % 2017-01-26
+
+clear all, close all, clc
+
+load('dataset1.mat');
+
+sigma_ml = length(x).*var(x);
+sigma_ml= mean(sigma_ml);
+
+prior_distribution = @(s,alpha,beta) beta.^alpha/gamma(alpha)*s.^(-alpha-1).*exp(-beta./s);
+posterior_distribution =@(s,alpha,beta) prior_distribution(s,alpha+1,beta+sqrt(sigma_ml));
+
+
+x_axis = 0:0.01:10;
+
+plot(x_axis,prior_distribution(x_axis,1,1))
+hold on
+plot(x_axis,posterior_distribution(x_axis,1,1))
+figure
+plot(x_axis,prior_distribution(x_axis,10,1))
+hold on
+plot(x_axis,posterior_distribution(x_axis,10,1))
